76f2deb94cd9738b174a923d678e38035d0b80e2
1q23lyc45 <174172451+1q23lyc45@users.noreply.github.com>
2026-02-08 00:01:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,4 @@
 
 # Add a feed source
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git luci-theme-argon https://github.com/1q23lyc45/luci-theme-argon' >>feeds.conf.default
+echo 'src-git argon https://github.com/1q23lyc45/luci-theme-argon.git' >>feeds.conf.default
